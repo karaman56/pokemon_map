b0df91c1e2e003c38a5d598fba2b936768aed5fe
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-02-11 23:57:48 +0500

    add Pokemon.image model field.
     add Pokemon.image model field.

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,7 +1,15 @@
+from django.utils.safestring import mark_safe
 from django.contrib import admin
-
 from .models import Pokemon
 
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image_display')  # Отображаем название и изображение
+    readonly_fields = ('image_display',)  # Делаем поле только для чтения
 
-admin.site.register(Pokemon)
+    def image_display(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="150" height="150" />')
+        return "Изображение отсутствует"
 
+    image_display.short_description = 'Фото'  # Заголовок для колонки
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='pokemons/', blank=True, null=True)
 
     def __str__(self):
         return f'{self.title}'
